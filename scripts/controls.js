01f36439b3e4c5dafdd0149b657d347c8a0cfa5a
heika201 <x.hei.x@Hotmail.com>
2015-07-03 21:30:04 -0700

    chat controls functions

---
+++ scripts/controls.js
@@ -0,0 +1,50 @@
+/*------------------------------------------------------------------------------
+Chat Controls
+------------------------------------------------------------------------------*/
+// Contacts-Button Controls
+$('#contacts-btn').on('click', function(e) {
+		$('.settings-container').css('display', 'none');
+		$('.container').css('display', 'none');
+        $('.contacts-container').css('display', 'block');
+});
+
+// Home-Button Controls
+$('#home-btn').on('click', function(e) {
+		$('.settings-container').css('display', 'none');
+        $('.contacts-container').css('display', 'none');
+		// Show chat box
+    $('.container').css('display', 'block');
+});
+
+$('#home-btn').one('click', function(e) {
+  // Display Recipient at top of chat-box
+  $('.panel-heading').html(global_recipient);
+});
+
+// Settings-Button Controls
+$('#settings-btn').on('click', function(e) {
+		$('.container').css('display', 'none');
+        $('.contacts-container').css('display', 'none');
+		$('.settings-container').css('display', 'block');
+});
+
+$('#send-msg-btn').on('click', function(e) {
+    // Q:Is this needed?
+    e.preventDefault();
+    clearError();
+
+    var text = $('input#btn-input').val();
+
+    $('#userMsg').tmpl({'username': global_username, 'time': timeStamp() , 'msg': text}).appendTo('.chat');
+    $('.panel-body').scrollTop($('.panel-body')[0].scrollHeight);
+
+    // Clears input?
+    $('input#btn-input').val('');
+
+    var sinchMessage = messageClient.newMessage(global_recipient, text);
+    //console.log('message object to be sent:' + sinchMessage.recipientIds);
+
+    messageClient.send(sinchMessage)
+        .then(function() {console.log('Message Sent');})
+        .fail(function() {console.log('Message Failed');});
+});
