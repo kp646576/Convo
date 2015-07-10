ca22e31567f00a52370ac5bac171d7a972e88b1b
KP <kp@KPs-MacBook-Pro.local>
2015-07-10 15:14:45 -0700

    Initial grunt commit for jsbeautifier.

---
+++ Gruntfile.js
@@ -0,0 +1,47 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    "jsbeautifier": {
+      files: ["./*.js", "./scripts/*.js"],
+      options: {
+        //config: "path/to/configFile",
+        html: {
+          braceStyle: "collapse",
+          indentChar: " ",
+          indentScripts: "keep",
+          indentSize: 2,
+          maxPreserveNewlines: 10,
+          preserveNewlines: true,
+          unformatted: ["a", "sub", "sup", "b", "i", "u"],
+          wrapLineLength: 0
+        },
+        css: {
+          indentChar: " ",
+          indentSize: 2
+        },
+        js: {
+          braceStyle: "collapse",
+          breakChainedMethods: false,
+          e4x: false,
+          evalCode: false,
+          indentChar: " ",
+          indentLevel: 0,
+          indentSize: 2,
+          indentWithTabs: false,
+          jslintHappy: false,
+          keepArrayIndentation: false,
+          keepFunctionIndentation: false,
+          maxPreserveNewlines: 10,
+          preserveNewlines: true,
+          spaceBeforeConditional: true,
+          spaceInParen: false,
+          unescapeStrings: false,
+          wrapLineLength: 0,
+          endWithNewline: true
+        }
+      }
+    },
+  });
+  grunt.loadNpmTasks("grunt-jsbeautifier");
+  grunt.registerTask('default', ['jsbeautifier']);
+};
